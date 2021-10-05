df417a72ef14713f78f0f8371775fc33940b757f
Restyled.io <commits@restyled.io>
2021-10-05 17:30:51 +0000

    Restyled by autopep8

--- sieve_of_erathosenes.py
+++ sieve_of_erathosenes.py
@@ -1,13 +1,14 @@
-#Sieve of Erathosenes
+# Sieve of Erathosenes
 def sieve(num):
-    l=[]
-    for i in range(2,num+1):
+    l = []
+    for i in range(2, num+1):
         l.append(i)
-    
-    for i in  l:
-        for  j in range(2,num//2):
+
+    for i in l:
+        for j in range(2, num//2):
             if i*j in l:
                 l.remove(i*j)
     print(l)
+
 
 sieve(50)
