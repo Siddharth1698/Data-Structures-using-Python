a3bdaa9fb2ce94a13775aa4e495f7fddf5ea7e20
Restyled.io <commits@restyled.io>
2021-10-05 17:40:46 +0000

    Restyled by black

--- shortest_job_first.py
+++ shortest_job_first.py
@@ -4,8 +4,8 @@ def schedule(l):
     l.sort()
     x = 0
     for i in range(0, len(l)):
-        x = x+l[i]
-        print("Job", i+1, " completed in", x)
+        x = x + l[i]
+        print("Job", i + 1, " completed in", x)
 
 
 schedule([5, 3, 2, 6, 11, 4])
