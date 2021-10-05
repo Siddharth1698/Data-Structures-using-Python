e0faf15918a0dc033a4cc7593cdf135b0e968068
Restyled.io <commits@restyled.io>
2021-10-05 17:40:42 +0000

    Restyled by autopep8

--- shortest_job_first.py
+++ shortest_job_first.py
@@ -1,10 +1,11 @@
-#CPU scheduling
-#Non preemptive sjf
+# CPU scheduling
+# Non preemptive sjf
 def schedule(l):
     l.sort()
-    x=0
-    for i in range(0,len(l)):
-        x=x+l[i]
-        print("Job",i+1," completed in",x)
+    x = 0
+    for i in range(0, len(l)):
+        x = x+l[i]
+        print("Job", i+1, " completed in", x)
 
-schedule([5,3,2,6,11,4])
+
+schedule([5, 3, 2, 6, 11, 4])
